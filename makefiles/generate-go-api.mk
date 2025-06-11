0148759770019a436e8eb14889245a65ff536aa0
Sokol111 <igorsokol111@gmail.com>
2025-06-12 01:07:44 +0200

    modified

--- makefiles/generate-go-api.mk
+++ makefiles/generate-go-api.mk
@@ -1,9 +1,28 @@
-OAPI_GEN := $(HOME)/go/bin/oapi-codegen
-OPENAPI_FILE := openapi/openapi.yml
-GEN_DIR := api
-PACKAGE := api
+export OPENAPI_FILE GEN_DIR PACKAGE
 
-.PHONY: install-tools create-gen-dir types server client generate-go-api clean
+REQUIRED_VARS := OPENAPI_FILE GEN_DIR PACKAGE
+
+OAPI_GEN := $(HOME)/go/bin/oapi-codegen
+
+.PHONY: check-vars print-vars install-tools create-gen-dir types server client generate-go-api clean
+
+check-vars:
+	@missing=""; \
+	for var in $(REQUIRED_VARS); do \
+		if [ -z "$${!var}" ]; then \
+			echo "❌ ERROR: $$var is not set"; \
+			missing=1; \
+		fi; \
+	done; \
+	if [ "$$missing" = "1" ]; then \
+		echo "💡 Tip: you can set variables via environment"; \
+		exit 1; \
+	fi
+
+print-vars:
+	@echo "OPENAPI_FILE		= $(OPENAPI_FILE)"
+	@echo "GEN_DIR        	= $(GEN_DIR)"
+	@echo "PACKAGE      	= $(PACKAGE)"
 
 install-tools:
 	@which oapi-codegen >/dev/null || (echo "Installing Go tools..."; \
@@ -24,7 +43,7 @@ client: install-tools create-gen-dir
 	@echo "Generating Go client..."
 	$(OAPI_GEN) -generate client -package $(PACKAGE) -o $(GEN_DIR)/client.gen.go $(OPENAPI_FILE)
 
-generate-go-api: clean types server client
+generate-go-api: check-vars print-vars clean types server client
 	@echo "Go API generated successfully."
 
 clean:
